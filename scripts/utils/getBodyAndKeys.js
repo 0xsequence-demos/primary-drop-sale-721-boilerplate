ac5e3b1e881b05164ba70ef3189e1a68d9c5b574
Richard Irala <richardirala47@hotmail.com>
2024-10-25 22:22:25 -0300

    Move some scripts key to .env

--- scripts/utils/getBodyAndKeys.js
+++ scripts/utils/getBodyAndKeys.js
@@ -1,14 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export default function getBodyAndKeys() {
   // "IMPORTANT: Make sure to modify the keys and the request body before using this script."
 
   const keys = {
     // Your project id can be found at https://sequence.build. You’ll see it in the URL after selecting your project
-    projectId: 893218998,
+    projectId: process.env.PROJECT_ID,
     // Follow the first step in https://docs.sequence.xyz/guides/metadata-guide/
-    jwtAccessKey: "",
+    jwtAccessKey: process.env.JWT_ACCESS_KEY,
     // Your access key can be found at https://sequence.build under the project settings
-    projectAccessKey: "",
+    projectAccessKey: process.env.VITE_PROJECT_ACCESS_KEY,
   };
+
+  if (!keys.projectId) throw new Error("Missing project id");
+  if (!keys.jwtAccessKey) throw new Error("Missing jwt access key");
+  if (!keys.projectAccessKey) throw new Error("Missing project access key");
 
   const body = {
     // Modify with number of NFTs to create
